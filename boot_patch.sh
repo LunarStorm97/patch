69292d77d353834326fb2139994e2052cd2fb9cb
LunarStorm97 <ian8388382@proton.me>
2024-12-21 23:55:14 -0300

    Update boot_patch.sh

--- boot_patch.sh
+++ boot_patch.sh
@@ -16,12 +16,12 @@ lz4 "boot.img.lz4" "boot.img"
 # Binary Patches
 #################
 
-# Remove Samsung RKP
+# Remove Real-time Kernel Protection (RKP)
 ./libmagiskboot.so hexpatch "kernel" \
   49010054011440B93FA00F71E9000054010840B93FA00F7189000054001840B91FA00F7188010054 \
   A1020054011440B93FA00F7140020054010840B93FA00F71E0010054001840B91FA00F7181010054
 
-# Remove Samsung defex
+# Remove Defeat Exploit (DEFEX)
 # Before: [mov w2, #-221]   (-__NR_execve)
 # After:  [mov w2, #-32768]
 ./libmagiskboot.so hexpatch "kernel" 821B8012 E2FF8F12
